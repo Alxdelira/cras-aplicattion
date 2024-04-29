2295bfe0d8b1648db511bc745fb681775bc410a1
Alexandre Nogueira de Lira <alx.delira@gmail.com>
2024-04-29 11:06:06 -0400

    feat: Adiciona pastas e conexão com o banco de dados Resolve #01

---
+++ server.js
@@ -0,0 +1,12 @@
+import app from './src/app.js';
+import * as dotenv from 'dotenv';
+
+
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import * as dotenv from 'dotenv';
+import express from 'express';
+
+
+
+dotenv.config();
+
+const routes = (app) => {
+    app.get('/', (req, res) => {
+        res.send('Welcome to the homepage');
+    });
+}
+
+
+// destinado as rotas a sertem criadas depois usando app.use('/api', routes); ex.
+
+export default routes;
